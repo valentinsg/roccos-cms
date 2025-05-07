a087db40b3910b0aced3033a9ae6147f0016c4f6
valentinsg <sanchezguevaravalentin@gmail.com>
2025-05-07 09:46:59 -0300

    Commit inicial

---
+++ schemaTypes/category.ts
@@ -0,0 +1,43 @@
+const category = {
+  name: "category",
+  title: "Categoría",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Nombre",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Descripción",
+      type: "text",
+    },
+    {
+      name: "image",
+      title: "Imagen",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "order",
+      title: "Orden",
+      type: "number",
+    },
+    {
+      name: "sucursal",
+      title: "Sucursal",
+      type: "string",
+      options: {
+        list: [
+          { title: "Rocco's", value: "roccos" },
+          { title: "Rocco's 2.0", value: "roccos-2" },
+        ],
+      },
+    },
+  ],
+}
+
+export default category
---
+++ schemaTypes/product.ts
@@ -0,0 +1,69 @@
+const product = {
+  name: 'product',
+  title: 'Producto',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Nombre',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Descripción',
+      type: 'text',
+    },
+    {
+      name: 'price',
+      title: 'Precio',
+      type: 'number',
+    },
+    {
+      name: 'category',
+      title: 'Categoría',
+      type: 'reference',
+      to: [{ type: 'category' }],
+    },
+    {
+      name: 'sucursal',
+      title: 'Sucursal',
+      type: 'string',
+      options: {
+        list: [
+          { title: "Rocco's", value: 'roccos' },
+          { title: "Rocco's 2.0", value: 'roccos-2' },
+        ],
+      },
+    },
+    {
+      name: 'isAvailable',
+      title: 'Disponible',
+      type: 'boolean',
+      initialValue: true,
+      description: 'Indica si el producto está disponible para la venta'
+    },
+    {
+      name: 'isPromo',
+      title: '¿Es promoción?',
+      type: 'boolean',
+    },
+    {
+      name: 'promoDetails',
+      title: 'Detalles de la promo',
+      type: 'string',
+    },
+    {
+      name: 'originalPrice',
+      title: 'Precio original',
+      type: 'number',
+    },
+    {
+      name: 'star',
+      title: 'Producto estrella',
+      type: 'boolean',
+      initialValue: false,
+    },
+  ],
+}
+
+export default product
